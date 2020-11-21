4a0cd3837b9cf79b7e9d22c85217e3edf56598cf
Jonathan Ramirez <jonathanalejandro.ramirezislas@gmail.com>
2020-11-20 22:52:54 -0600

    delete Favorite

--- redux/favorites.js
+++ redux/favorites.js
@@ -3,13 +3,17 @@ import * as ActionTypes from './ActionTypes';
 //reducer
 export const favorites = (state = [], action) => {
     switch (action.type) {
+
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter((favorite) => favorite !== action.payload);
+        
         case ActionTypes.ADD_FAVORITE:
             //if the dish is already in the db
             if (state.some(el => el === action.payload))
                 return state;//just return the state
             else
                 return state.concat(action.payload);//add a new dish to favorites
-                
+
         default:
           return state;
       }
