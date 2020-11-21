24b9bc1cb98e651716999b7be2c8b9a1e8051ca1
Jonathan Ramirez <jonathanalejandro.ramirezislas@gmail.com>
2020-11-21 15:24:43 -0600

    animation

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -33,13 +34,15 @@ function RenderComments(props) {
     };
     
     return (
-        <Card title='Comments' >
-        <FlatList 
-            data={comments}
-            renderItem={renderCommentItem}
-            keyExtractor={item => item.id.toString()}
-            />
-        </Card>
+        <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>        
+            <Card title='Comments' >
+                <FlatList 
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            </Card>
+        </Animatable.View>
     );
 }
 
@@ -49,6 +52,7 @@ function RenderDish(props) {
     
         if (dish != null) {
             return(
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
                 <Card
                 featuredTitle={dish.name}
                 image={{uri: baseUrl + dish.image}}>
@@ -64,6 +68,8 @@ function RenderDish(props) {
                         onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
                         />
                 </Card>
+                </Animatable.View>
+
             );
         }
         else {
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, View, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -45,35 +45,69 @@ const mapStateToProps = state => {
 class Home extends Component {
     constructor(props) {
         super(props);
+        this.animatedValue=new Animated.Value(0);
       }
     static navigationOptions = {
         title: 'Home',
     };
 
-    render() {
-        
-        return(
-            <ScrollView>
-                {/*featured with value true in data, those will appear only and with [0] we extract the first objet(item)  */}
-                <RenderItem
-                    item={this.props.dishes.dishes.filter(dish => dish.featured)[0]}
-                    isLoading={this.props.dishes.isLoading}
-                    errMess={this.props.dishes.errMess}
+    componentDidMount () {
+      this.animate()
+  }
+
+  animate () {
+      this.animatedValue.setValue(0);
+      /*
+      specify parameters  duration and mode of animation
+      */
+      Animated.timing(
+        this.animatedValue,
+        {
+          toValue: 8,// from 0 to 8. NOTE this will be used in xpos1,xpos2,xpos3
+          duration: 8000,//duration 8 seconds
+          easing: Easing.linear //way to change the value in this case linear
+        }
+      ).start(() => this.animate());//start the animation
+  }
+
+  render() {
+    //interpolate means map
+    const xpos1 = this.animatedValue.interpolate({
+        inputRange: [0, 1, 3, 5, 8],//ASC Values when x =0 then value of dregres will be 1200 and so on
+        outputRange: [1200, 600, 0, -600, -1200] //axis x (pixels)
+    })
+    const xpos2 = this.animatedValue.interpolate({
+        inputRange: [0, 2, 4, 6, 8],
+        outputRange: [1200, 600, 0, -600, -1200]
+    })
+    const xpos3 = this.animatedValue.interpolate({
+        inputRange: [0, 3, 5, 7, 8],
+        outputRange: [1200, 600, 0, -600, -1200 ]
+    })
+
+    return(
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+        <Animated.View style={{ width: '100%', transform: [{translateX: xpos1}]}}>
+            <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                isLoading={this.props.dishes.isLoading}
+                erreMess={this.props.dishes.erreMess} 
                 />
-               <RenderItem
-                    item={
-                    this.props.promotions.promotions.filter(promo => promo.featured)[0]
-                    }
-                    isLoading={this.props.promotions.isLoading}
-                    errMess={this.props.promotions.errMess}
-                />              
-                <RenderItem
-                    item={this.props.leaders.leaders.filter(leader => leader.featured)[0]}
-                    isLoading={this.props.leaders.isLoading}
-                    errMess={this.props.leaders.errMess}
-                    />
-            </ScrollView>
-        );
+        </Animated.View>
+        <Animated.View style={{ width: '100%',  transform: [{translateX: xpos2}]}}>
+            <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                isLoading={this.props.promotions.isLoading}
+                erreMess={this.props.promotions.erreMess} 
+                />
+        </Animated.View>
+        <Animated.View style={{ width: '100%',  transform: [{translateX: xpos3}]}}>
+            <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                isLoading={this.props.leaders.isLoading}
+                erreMess={this.props.leaders.erreMess} 
+                />
+            </Animated.View>
+        </View>
+    );
+ 
     }
 }
 
